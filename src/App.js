c73acd456527b44db1b444b1a2e4feefabbeeb9a
Aszmel <aszmel@o2.pl>
2019-02-14 20:49:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <div className="form-wrapper">
+          <h1>Create Account</h1>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                noValidate
+                className=""
+                name="firstName"
+                placeholder="First Name"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                noValidate
+                className=""
+                name="lastName"
+                placeholder="Last Name"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                noValidate
+                className=""
+                name="email"
+                placeholder="email"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                password
+                noValidate
+                className=""
+                name="password"
+                placeholder="password"
+                onChange={this.handleChange}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
